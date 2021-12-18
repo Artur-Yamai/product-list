4eca4eecd598a3c4f2f99bed30dc5011247440f4
ArturYamaletdinov <wbf.robit@gmail.com>
2021-12-18 18:08:04 +0500

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="header">      
+    </div>
+</template>
+
+<style lang="scss">
+.header {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 16px;
+
+    h1 {
+        color: #3F3F3F;
+        font-weight: bolder;
+        font-size: 28px;
+        line-height: 35px;
+    }
+}
+</style>
