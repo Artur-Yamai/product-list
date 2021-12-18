bb01e6ee3d1f3ab7d894f518e21d1787a3b6f3f2
ArturYamaletdinov <wbf.robit@gmail.com>
2021-12-18 18:08:59 +0500

    init

---
+++ src/components/ProductList.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul class="product-list">
+    </ul>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+.product-list {
+    width: 75%;
+    margin-left: 16px;
+}
+</style>
