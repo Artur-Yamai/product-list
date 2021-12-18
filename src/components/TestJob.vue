96fe052f768f74c083577b5cbaace04fb500a9c2
ArturYamaletdinov <wbf.robit@gmail.com>
2021-12-18 18:09:32 +0500

    init

---
+++ src/components/TestJob.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="test-job">
+        <Header/>
+        <div class="test-job__wrapper">
+            <FormAddProducts/>
+            <ProductList/>
+        </div>
+    </div>
+</template>
+
+<script>
+import Header from './Header';
+import FormAddProducts from './FormAddProducts';
+import ProductList from './ProductList';
+
+export default {
+    components: {
+        Header,
+        FormAddProducts,
+        ProductList
+    }    
+}
+</script>
+
+<style lang="scss">
+.test-job {    
+  padding: 32px;
+
+  &__wrapper {
+      display: flex;
+      justify-content: space-between;
+  }
+}
+</style>
