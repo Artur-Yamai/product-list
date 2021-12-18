e7ccc7463ffbae6ab483b312ab7d3cdd0a042136
ArturYamaletdinov <wbf.robit@gmail.com>
2021-12-18 18:07:35 +0500

    init

---
+++ src/components/FormAddProducts.vue
@@ -0,0 +1,22 @@
+<template>
+    <form class="add-products-form">
+    </form>
+</template>
+
+<script>
+
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+.add-products-form {
+    width: calc(25%);
+    height: 440px;
+    padding: 24px;
+    background: #FFFEFB;
+    box-shadow: 0px 20px 30px rgba(0, 0, 0, 0.04), 0px 6px 10px rgba(0, 0, 0, 0.02);
+    border-radius: 4px;
+}
+</style>
