1cd5b7b044c49355d78e2cddb7ac7ace6992aef0
ArturYamaletdinov <wbf.robit@gmail.com>
2021-12-19 21:12:29 +0500

    добавлена возмодность сортировки товаров

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,28 @@
 <template>
-    <div class="header">      
+    <div class="header">        
+        <h1>Добавление товара</h1>
+        <select v-model="selected">
+            <option value="default" default>По умолчанию</option>
+            <option value="priceToMax">по цене &#8593;</option>
+            <option value="priceToMin">по цене &#8595;</option>
+        </select>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            selected: 'default',
+        }
+    },
+    watch: {
+        selected(val) {
+            this.$store.commit('toSortProductList', val)
+        }
+    }
+}
+</script>
 
 <style lang="scss">
 .header {
@@ -14,6 +35,35 @@
         font-weight: bolder;
         font-size: 28px;
         line-height: 35px;
+    }
+}
+
+select {
+    border: none;
+    background: #FFFEFB;
+    box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+    border-radius: 4px;
+    padding: 6px 8px;
+
+    font-style: normal;
+    font-weight: normal;
+    font-size: 12px;
+    line-height: 15px;
+    color: #B4B4B4;
+
+    &:focus {
+        border: 1px solid gray;
+    }
+
+    
+}
+
+option {
+    border: none;
+    padding: 4px;
+
+    &:focus {
+        background-color: red;
     }
 }
 </style>
